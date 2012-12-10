a4fe49518417139ce3338a4d8254eb49239946d0
Theeban Soundararajan <theeban@intersect.org.au>
2012-12-11 10:12:15 +1100

    Ordering projects list by name

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -24,16 +24,27 @@ describe Project do
     it { should allow_value("Project Test @Something").for(:name)}
 
     it "project names should be unique" do
-      t1 = FactoryGirl.build(:project, :name => "Project 1")
-      t2 = FactoryGirl.build(:project, :name => "Project 2")
-      t3 = FactoryGirl.build(:project, :name => "Project 1")
-      t4 = FactoryGirl.build(:project, :name => "Project    1")
-      t5 = FactoryGirl.build(:project, :name => "    Project 1    ")
-      t1.save.should == true
-      t2.save.should == true
-      t3.save.should == false
-      t4.save.should == false
-      t5.save.should == false
+      p1 = FactoryGirl.build(:project, :name => "Project 1")
+      p2 = FactoryGirl.build(:project, :name => "Project 2")
+      p3 = FactoryGirl.build(:project, :name => "Project 1")
+      p4 = FactoryGirl.build(:project, :name => "Project    1")
+      p5 = FactoryGirl.build(:project, :name => "    Project 1    ")
+      p1.save.should == true
+      p2.save.should == true
+      p3.save.should == false
+      p4.save.should == false
+      p5.save.should == false
     end
   end
+
+  describe "Should order by name" do
+    it do
+      p1 = FactoryGirl.create(:project, :name => "B Project")
+      p2 = FactoryGirl.create(:project, :name => "A Project")
+      p3 = FactoryGirl.create(:project, :name => "C Project")
+      projects = Project.all
+      projects.should == [p2, p1, p3]
+    end
+  end
+
 end
