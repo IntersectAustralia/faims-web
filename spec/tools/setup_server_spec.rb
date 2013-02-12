b5ce223292a7f4e1362beac8a1cd8b4276014a3a
Theeban Soundararajan <theeban@intersect.org.au>
2013-02-12 16:43:36 +1100

    FAIMS-185: Adding rake task to generate server properites file

---
+++ spec/tools/setup_server_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+load "#{Rails.root}/lib/tasks/setup_server.rb"
+
+describe "Server setup" do
+
+  it "generates properties file with uuid" do
+    file = Tempfile.new('server.properties')
+    create_server_properties(file.path)
+    uuid = file.read.gsub('server_key=', '')
+    parts = uuid.split('-')
+    count = 0
+    [8, 4, 4, 4, 12].each do |length|
+      parts.shift.length.should == length
+    end
+    file.unlink
+  end
+end
