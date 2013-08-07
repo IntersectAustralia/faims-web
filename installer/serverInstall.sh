a6c708777b76eb9068b0abfe04268138282a1b80
Brian Ballsun-Stanton <brian@fedarch.org>
2013-08-07 15:18:47 +1000

    new installer

--- installer/serverInstall.sh
+++ installer/serverInstall.sh
@@ -1,24 +1,41 @@
-#! /bin/bash
+#!/bin/bash
+
+
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Trying now..." 
+   clear
+   echo -e '\E[37;44m'"\033[1mLaunching Installer...\033[0m"
    cp serverInstall.sh /tmp/serverInstall.sh
-   gksudo -D "FAIMS server" -m "Please enter your password to install the FAIMS Server" "gnome-terminal -x bash /tmp/serverInstall.sh"
+   gksudo -D "FAIMS server" -m "Please enter your password to install the FAIMS Server" "bash /tmp/serverInstall.sh"
+   echo -n "Now launching faims server in a browser."
+   pgrep unicorn > /dev/null
+   while [ $? -ne 0 ]; do
+   	echo -n .
+   	sleep 1
+   	pgrep unicorn
+   done 
+   sensible-browser http://localhost:3000 &
    exit 1
 fi
 
+echo progress-bar >> ~/.curlrc
 
+echo -e '\E[37;44m'"\033[1mInstalling prgogress bars. Please wait.\033[0m"
+apt-get -qq -y install software-properties-common pv
+add-apt-repository ppa:faims/mobile-web -y >> /tmp/repo.log 2> /tmp/err
+add-apt-repository ppa:ubuntugis/ppa -y >> /tmp/repo.log 2>> /tmp/err
 
-apt-get install software-properties-common -y
-add-apt-repository ppa:faims/mobile-web -y
-add-apt-repository ppa:ubuntugis/ppa -y
+echo -e '\E[37;44m'"\033[1mUpdating Package List.\033[0m"
 apt-get update
 
-apt-get install build-essential g++ gawk libreadline6-dev libsqlite3-dev bison pkg-config git-core wget curl expat libexpat1-dev zlib1g-dev libyaml-dev libxslt1-dev libgdbm-dev libncurses5-dev libffi-dev d-shlibs dh-autoreconf libblas3gf libepsilon-dev liblapack3gf libogdi3.2-dev python-all python-all-dev python-central python-dev python-numpy python2.7-dev gdal-bin libgdal-doc libproj-dev libproj0 libgdal-dev libgeos-dev libgeos++-dev libfreexl-dev tcl tcl-dev libreadosm-dev sqlite3 -y
+echo -e '\E[37;44m'"\033[1mDownloading & Installing Packages\033[0m"
+apt-get install unzip build-essential g++ gawk libreadline6-dev libsqlite3-dev bison pkg-config git-core wget curl expat libexpat1-dev zlib1g-dev libyaml-dev libxslt1-dev libgdbm-dev libncurses5-dev libffi-dev d-shlibs dh-autoreconf libblas3gf libepsilon-dev liblapack3gf libogdi3.2-dev python-all python-all-dev python-central python-dev python-numpy python2.7-dev gdal-bin libgdal-doc libproj-dev libproj0 libgdal-dev libgeos-dev libgeos++-dev libfreexl-dev tcl tcl-dev libreadosm-dev sqlite3 -y
+
+echo "Upgrading System Packages."
 apt-get upgrade -y
 
+echo "Preparing to Compile Packages."
 apt-get build-dep spatialite spatialite-tools -y
-
 
 cd /tmp/
 
@@ -33,40 +50,52 @@ echo "export rvm_autolibs_flag=3" >> /etc/rvmrc
 
 cd /tmp/
 
-wget http://www.gaia-gis.it/gaia-sins/libspatialite-4.1.1.tar.gz 
-wget http://www.gaia-gis.it/gaia-sins/spatialite-tools-4.1.1.tar.gz 
+echo "Downloading Spatialite and its tools."
+wget http://www.fedarch.org/libspatialite-4.1.1.tar.gz
+wget http://www.fedarch.org/spatialite-tools-4.1.1.tar.gz 
 
 tar -xzf libspatialite-4.1.1.tar.gz
 tar -xzf spatialite-tools-4.1.1.tar.gz 
 
+echo "Compiling Stuff. This will take a while (×6)."
+
 cd /tmp/libspatialite-4.1.1
 
-./configure
-make > spatialiteBuild.log
-make install
+./configure | pv -p -s 9656 -e > /tmp/spatialiteBuildConfig.log
+make | pv -p -s 115351 -e > /tmp/spatialiteBuildMake.log
+make install | pv -p -s 15628 -e > /tmp/patialiteBuildMakeInstall.log
 
 cd /tmp/spatialite-tools-4.1.1
 
-./configure
-make > spatialiteToolsBuild.log
-make install
+./configure | pv -p -s 8514 -e > /tmp/spatialiteTestBuildConfig.log
+make | pv -p -s 8195 -e > /tmp/spatialiteToolsBuildMake.log
+make install | pv -p -s 1501 -e > /tmp/spatialiteBuildMakeInstall.log
 
-\curl -L https://get.rvm.io | bash -s stable --ruby=1.9.3-p286 --autolibs=3
+echo -e "Installing RVM. All instructions in the following text have been taken care of."
+\curl -# -L https://get.rvm.io | bash -s stable --ruby=1.9.3-p286 --autolibs=3  >out 2>err
 
+echo "Setting up correct groups and profiles."
+
+usermod -a -G rvm $SUDO_USER
+usermod -a -G rvm root
+
+
+# echo "RAILS_ENV=production" >> /etc/profile.d/rvm.sh
 source /etc/profile.d/rvm.sh
 
 echo "source /etc/profile.d/rvm.sh" >> /etc/bash.bashrc
 
 rvm requirements
 
-rvm use 1.9.3-p286@faims --create
+rvm 1.9.3-p286@faims --create 
 # install ruby-1.9.3-p286
 
 # rm -rf /opt/faims
 
 cd /opt/
 
-wget https://github.com/IntersectAustralia/faims-web/archive/master.zip
+echo "Downloading the FAIMS Server."
+curl http://www.fedarch.org/master.zip > master.zip
 
 unzip -q master.zip
 
@@ -74,24 +103,26 @@ mv /opt/faims-web-master /opt/faims-web
 rvm rvmrc warning ignore /opt/faims-web/.rvmrc
 
 
-chown -R $SUDO_USER:rvm /opt/faims-web
-
-chmod -R g+rw /opt/faims-web
-
 cd /opt/faims-web/
 
-bundle install
+rvm 1.9.3-p286@faims --create --rvmrc
 
-rake db:create db:migrate db:seed
+echo "Installing rvm gemsets. This will take a while."
+bundle install | pv -p -s 3847 -e > /tmp/bundle.log
+
+echo "Setting up Server. This will take a long while."
+rake db:create db:migrate db:seed assets:precompile projects:setup RAILS_ENV=production 2>&1 | pv -p > /tmp/rake.log 
 
 # chmod -R a+wx /opt/faims-web
 
-usermod -a -G rvm $SUDO_USER
-usermod -a -G rvm root
 
-foreman export upstart /etc/init -a faims-web -u $SUDO_USER
+foreman export upstart /etc/init -a faims-web -u $SUDO_USER -f Procfile.production
 
 start faims-web
 
-sed -i '1istart on runlevel 5' /etc/init/faims-web.conf
+chown -R $SUDO_USER:root /opt/faims-web
 
+chmod -R g+rw /opt/faims-web
+
+
+sed -i '1istart on runlevel 5' /etc/init/faims-web.conf
