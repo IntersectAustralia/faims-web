4859b7386f7e278fab28e526c33deec5f709979a
danieltheodosius <danielt@intersect.org.au>
2013-10-21 15:27:50 +1100

    Fix the rails vulnerability

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FaimsWeb::Application.config.secret_token = 'f015a6b5fe19f9946016a22e32145046f7d9b780e95de6ea44446823fa51ef92b318e85ca779c05ce32c1fb3b88959873dc19d02a5e1e6a0ecc0d23e2e87bcd1'
+FaimsWeb::Application.config.secret_token = 'REPLACE_THIS_WITH_REAL_GOOD_RANDOM_STRING'
