51fa66ca0a06dd122007a17576e9d52f822ae86a
Theeban Soundararajan <theeban@intersect.org.au>
2014-10-24 13:02:54 +1100

    FAIMS-859: updated apache2 user to have sudo access

--- puppet/modules/sudo_user/manifests/init.pp
+++ puppet/modules/sudo_user/manifests/init.pp
@@ -1,9 +1,7 @@
 class sudo_user {
   require common
 
-  $webapp_user = hiera("webapp_user")
-
-  file { "/etc/sudoers.d/${webapp_user}":
+  file { "/etc/sudoers.d/www-data":
     mode    => "0644",
     owner   => "root",
     group   => "root",
