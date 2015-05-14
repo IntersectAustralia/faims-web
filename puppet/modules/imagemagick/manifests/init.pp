eb0f24e5824d6d35a316fe1adf41b18014fdc2b0
Theeban Soundararajan <theeban@intersect.org.au>
2015-05-14 12:00:51 +1000

    FAIMS-916: update ppa for ffmpeg

--- puppet/modules/imagemagick/manifests/init.pp
+++ puppet/modules/imagemagick/manifests/init.pp
@@ -1,7 +1,7 @@
 class imagemagick {
   require common
 
-  apt::ppa { 'ppa:jon-severinsson/ffmpeg':
+  apt::ppa { 'ppa:mc3man/trusty-media':
   }
 
   $imagemagick_packages = ["imagemagick","libmagickwand-dev","ffmpeg","libmagickcore5-extra","ghostscript","netpbm","autotrace","html2ps","ufraw-batch","dcraw","transfig","libbz2-1.0"]
