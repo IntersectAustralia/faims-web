22849f6586e09c6ef67dc68ea0b798b2630e269e
Theeban Soundararajan <theeban@intersect.org.au>
2015-05-18 14:43:32 +1000

    FAIMS-916: fix bug with imagemagick sources not being updated before trying to install

--- puppet/modules/imagemagick/manifests/init.pp
+++ puppet/modules/imagemagick/manifests/init.pp
@@ -4,10 +4,17 @@ class imagemagick {
   apt::ppa { 'ppa:mc3man/trusty-media':
   }
 
+  exec { "update imagemagick sources":
+    path    => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin",
+    command => '/bin/true',
+    unless  => 'apt-get update',
+    timeout => 600
+  }
+
   $imagemagick_packages = ["imagemagick","libmagickwand-dev","ffmpeg","libmagickcore5-extra","ghostscript","netpbm","autotrace","html2ps","ufraw-batch","dcraw","transfig","libbz2-1.0"]
   package { $imagemagick_packages:
     ensure  => "present",
-    require => [Apt::Ppa['ppa:mc3man/trusty-media'],Exec["update sources"]]
+    require => [Apt::Ppa['ppa:mc3man/trusty-media'],Exec["update imagemagick sources"]]
   }
 
 }
